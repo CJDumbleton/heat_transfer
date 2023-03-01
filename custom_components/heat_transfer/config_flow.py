467d321c45db4a0ac63cac99966754c7f251fff5
CJ Dumbleton <55947036+CJDumbleton@users.noreply.github.com>
2023-03-01 20:43:35 +0000

    Basic config

--- custom_components/heat_transfer/config_flow.py
+++ custom_components/heat_transfer/config_flow.py
@@ -3,17 +3,21 @@ from __future__ import annotations
 
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
-from homeassistant.helpers import selector
-from homeassistant.helpers.aiohttp_client import async_create_clientsession
-
-from .api import (
-    IntegrationBlueprintApiClient,
-    IntegrationBlueprintApiClientAuthenticationError,
-    IntegrationBlueprintApiClientCommunicationError,
-    IntegrationBlueprintApiClientError,
+from homeassistant.components.input_number import (
+    DOMAIN as INPUT_NUMBER_DOMAIN
 )
-from .const import DOMAIN, LOGGER
+from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
+from homeassistant.const import CONF_NAME
+from homeassistant.helpers import selector
+import homeassistant.helpers.config_validation as cv
+
+from .const import (
+    CONF_IN_T_SENSOR,
+    CONF_OUT_T_SENSOR,
+    DOMAIN,
+    LOGGER,
+    UnknownEntity,
+)
 
 
 class HeatTransferFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
@@ -29,22 +33,15 @@ class HeatTransferFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         _errors = {}
         if user_input is not None:
             try:
-                await self._test_credentials(
-                    username=user_input[CONF_USERNAME],
-                    password=user_input[CONF_PASSWORD],
-                )
-            except IntegrationBlueprintApiClientAuthenticationError as exception:
-                LOGGER.warning(exception)
-                _errors["base"] = "auth"
-            except IntegrationBlueprintApiClientCommunicationError as exception:
-                LOGGER.error(exception)
-                _errors["base"] = "connection"
-            except IntegrationBlueprintApiClientError as exception:
-                LOGGER.exception(exception)
+                await self._validate_input(user_input)
+            except UnknownEntity as err:
+                _errors[str(err)] = "unknown_entity"
+            except Exception:  # pylint: disable=broad-except
+                LOGGER.exception("Unexpected exception")
                 _errors["base"] = "unknown"
             else:
                 return self.async_create_entry(
-                    title=user_input[CONF_USERNAME],
+                    title=user_input[CONF_NAME],
                     data=user_input,
                 )
 
@@ -52,29 +49,32 @@ class HeatTransferFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="user",
             data_schema=vol.Schema(
                 {
-                    vol.Required(
-                        CONF_USERNAME,
-                        default=(user_input or {}).get(CONF_USERNAME),
-                    ): selector.TextSelector(
-                        selector.TextSelectorConfig(
-                            type=selector.TextSelectorType.TEXT
+                    vol.Required(CONF_NAME): cv.string,
+                    vol.Required(CONF_IN_T_SENSOR): selector.EntitySelector(
+                        selector.EntitySelectorConfig(
+                            domain=[SENSOR_DOMAIN, INPUT_NUMBER_DOMAIN]
                         ),
                     ),
-                    vol.Required(CONF_PASSWORD): selector.TextSelector(
-                        selector.TextSelectorConfig(
-                            type=selector.TextSelectorType.PASSWORD
+                    vol.Required(CONF_OUT_T_SENSOR): selector.EntitySelector(
+                        selector.EntitySelectorConfig(
+                            domain=[SENSOR_DOMAIN, INPUT_NUMBER_DOMAIN]
                         ),
-                    ),
+                    )
                 }
             ),
             errors=_errors,
         )
 
-    async def _test_credentials(self, username: str, password: str) -> None:
-        """Validate credentials."""
-        client = IntegrationBlueprintApiClient(
-            username=username,
-            password=password,
-            session=async_create_clientsession(self.hass),
-        )
-        await client.async_get_data()
+    async def _validate_input(self, data: dict) -> None:
+        """Validate the user's inputs are usable entities."""
+        for conf in [
+            CONF_IN_T_SENSOR,
+            CONF_OUT_T_SENSOR,
+        ]:
+            d = data.get(conf, None)  # pylint: disable=invalid-name
+            if input is not None and self.hass.states.get(d) is None:
+                LOGGER.error(
+                    "Error: entity id %s doesn't have any state.",
+                    d,
+                )
+                raise UnknownEntity(conf)
--- custom_components/heat_transfer/const.py
+++ custom_components/heat_transfer/const.py
@@ -1,9 +1,16 @@
 """Constants for heat_transfer."""
 from logging import Logger, getLogger
 
+from homeassistant.exceptions import HomeAssistantError
+
 LOGGER: Logger = getLogger(__package__)
 
 NAME = "Heat Transfer"
 DOMAIN = "heat_transfer"
 VERSION = "0.0.0"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
+
+CONF_IN_T_SENSOR = "in_temp_sensor_entity_id"
+CONF_OUT_T_SENSOR = "out_temp_sensor_entity_id"
+
+class UnknownEntity(HomeAssistantError):
+    """Error to indicate there is an unknown entity_id given."""
